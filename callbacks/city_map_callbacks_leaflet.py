919ad09c72ed9db4c972b4d6e4ca0ab1998725b4
Ahmed Essam <161029037+AhmeDvEssam@users.noreply.github.com>
2025-12-12 23:51:22 +0200

    Debug: Disable Custom JS Renderer to check data

--- callbacks/city_map_callbacks_leaflet.py
+++ callbacks/city_map_callbacks_leaflet.py
@@ -595,7 +595,7 @@ def update_city_map(companies, cities, categories, work_modes, job_statuses, emp
                         data=geojson_data,
                         cluster=True,
                         zoomToBoundsOnClick=True,
-                        options=dict(pointToLayer=ns("renderMarker")), # Use JS Function
+                        # options=dict(pointToLayer=ns("renderMarker")), # Use JS Function -> DISABLED FOR DEBUGGING
                         id="city-geojson-layer"
                     )
                 ]
